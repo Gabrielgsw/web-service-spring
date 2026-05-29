626f049d1efddf61d07ed39e22f9ddbd915300be
Gabrielgsw <ggermanow279@gmail.com>
2026-05-29 00:26:40 -0300

    feat: adicionando exceção padrão e 404

---
+++ src/main/java/br/com/calcassinc/webservice/exception/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package br.com.calcassinc.webservice.exception;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice //tratar possíveis erros nas requisições
+
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
